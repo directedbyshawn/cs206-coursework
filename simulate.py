c62679a61ef524f5d27cd033a240e61865560dfd
Shawn Rodgers <me@directedbyshawn.com>
2023-02-06 16:34:23 -0500

    saved sensor values to file

--- simulate.py
+++ simulate.py
@@ -1,6 +1,8 @@
 import pybullet as p
 import time as t
 import pybullet_data
+import pyrosim.pyrosim as pyrosim
+import numpy as np
 
 physicsClient = p.connect(p.GUI)
 
@@ -12,9 +14,15 @@ planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 
+pyrosim.Prepare_To_Simulate(robotId)
+
+backlegSensorValues = np.zeros(1000)
+
 for i in range(1000):
     p.stepSimulation()
-    print(i)
+    backlegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     t.sleep(1/60)
+
+np.save('data/bl_sensor_values.npy', backlegSensorValues)
 
 p.disconnect()
