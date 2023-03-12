b7de5e59706e5eb4b23f4acfd8b6daa20240d1a0
Shawn Rodgers <me@directedbyshawn.com>
2023-03-12 18:16:21 -0400

    step 19

--- search.py
+++ search.py
@@ -1,7 +1,11 @@
 import os
 from time import sleep
+from hillclimber import HILL_CLIMBER
+from solution import SOLUTION
 
-for i in range(5):
-    os.system('python generate.py')
-    sleep(1)
-    os.system('python simulation.py')
+hc = HILL_CLIMBER()
+
+# for i in range(5):
+#     os.system('python generate.py')
+#     sleep(1)
+#     os.system('python simulation.py')
